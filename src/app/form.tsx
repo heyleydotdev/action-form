cfedcf2b89d46208f6431d76412403d39ba3ac81
MrHeyLey <maxshashi29@gmail.com>
2024-11-26 18:24:28 +0530

    validation message

--- src/app/form.tsx
+++ src/app/form.tsx
@@ -16,6 +16,9 @@ export default function Form() {
               <FormItem>
                 <FormLabel>Text Input</FormLabel>
                 <input type='text' placeholder='Text Input' defaultValue={state?.prevValues?.text} {...attr} />
+                {state?.fieldErrors?.text?.[0] && (
+                  <p className='text-red-600 text-sm font-medium'>{state.fieldErrors.text[0]}</p>
+                )}
               </FormItem>
             )}
           </FormField>
@@ -24,36 +27,60 @@ export default function Form() {
               <FormItem>
                 <FormLabel>Date Input</FormLabel>
                 <input type='date' defaultValue={state?.prevValues?.date?.toISOString().slice(0, 10)} {...attr} />
+                {state?.fieldErrors?.date?.[0] && (
+                  <p className='text-red-600 text-sm font-medium'>{state.fieldErrors.date[0]}</p>
+                )}
               </FormItem>
             )}
           </FormField>
-          <FormField name='radio'>
-            {(attr) => (
-              <FormLabel>
-                <input type='radio' value='option1' defaultChecked={state?.prevValues?.radio === "option1"} {...attr} />{" "}
-                Option 1
-              </FormLabel>
+          <FormItem>
+            <FormField name='radio'>
+              {(attr) => (
+                <FormLabel>
+                  <input
+                    type='radio'
+                    value='option1'
+                    defaultChecked={state?.prevValues?.radio === "option1"}
+                    {...attr}
+                  />{" "}
+                  Option 1
+                </FormLabel>
+              )}
+            </FormField>
+            <FormField name='radio'>
+              {(attr) => (
+                <FormLabel>
+                  <input
+                    type='radio'
+                    value='option2'
+                    defaultChecked={state?.prevValues?.radio === "option2"}
+                    {...attr}
+                  />{" "}
+                  Option 2
+                </FormLabel>
+              )}
+            </FormField>
+            {state?.fieldErrors?.radio?.[0] && (
+              <p className='text-red-600 text-sm font-medium'>{state.fieldErrors.radio[0]}</p>
             )}
-          </FormField>
-          <FormField name='radio'>
-            {(attr) => (
-              <FormLabel>
-                <input type='radio' value='option2' defaultChecked={state?.prevValues?.radio === "option2"} {...attr} />{" "}
-                Option 2
-              </FormLabel>
-            )}
-          </FormField>
+          </FormItem>
           <FormField name='checkbox'>
             {(attr) => (
-              <FormLabel>
-                <input
-                  type='checkbox'
-                  value='option1'
-                  defaultChecked={state?.prevValues?.checkbox === "option1"}
-                  {...attr}
-                />{" "}
-                Checkbox
-              </FormLabel>
+              <FormItem>
+                <FormLabel>
+                  <input
+                    type='checkbox'
+                    value='option1'
+                    defaultChecked={state?.prevValues?.checkbox === "option1"}
+                    {...attr}
+                  />{" "}
+                  Checkbox
+                </FormLabel>
+
+                {state?.fieldErrors?.checkbox?.[0] && (
+                  <p className='text-red-600 text-sm font-medium'>{state.fieldErrors.checkbox[0]}</p>
+                )}
+              </FormItem>
             )}
           </FormField>
           <FormField name='select'>
@@ -65,6 +92,9 @@ export default function Form() {
                   <option value='option2'>Option 2</option>
                   <option value='option3'>Option 3</option>
                 </select>
+                {state?.fieldErrors?.select?.[0] && (
+                  <p className='text-red-600 text-sm font-medium'>{state.fieldErrors.select[0]}</p>
+                )}
               </FormItem>
             )}
           </FormField>
@@ -79,6 +109,9 @@ export default function Form() {
                   defaultValue={state?.prevValues?.textarea}
                   {...attr}
                 />
+                {state?.fieldErrors?.textarea?.[0] && (
+                  <p className='text-red-600 text-sm font-medium'>{state.fieldErrors.textarea[0]}</p>
+                )}
               </FormItem>
             )}
           </FormField>
@@ -87,6 +120,9 @@ export default function Form() {
               <FormItem>
                 <FormLabel>Range Input</FormLabel>
                 <input type='range' min='0' max='100' defaultValue={state?.prevValues?.range} {...attr} />
+                {state?.fieldErrors?.range?.[0] && (
+                  <p className='text-red-600 text-sm font-medium'>{state.fieldErrors.range[0]}</p>
+                )}
               </FormItem>
             )}
           </FormField>
